30389886a50ce4a4aebe90cf00bc078d8a0ed5e0
josephktsai <josephkt@uw.edu>
2018-09-27 14:41:41 -0700

    Add project and screenshots

---
+++ INFO449HW1/INFO449HW1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INFO449HW1
+//
+//  Created by Joseph Khamsene Tsai on 9/27/18.
+//  Copyright © 2018 Joseph Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
